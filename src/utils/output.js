7581a5dee3b3f93d6c969c39e8925d3dc2c0ac6d
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-11-29 14:49:42 +0000

    make output pretty-ish

--- src/utils/output.js
+++ src/utils/output.js
@@ -1,7 +1,17 @@
+import chalk from 'chalk';
+
+export function priceChangeColor(change) {
+  return parseFloat(change) < 0 ? chalk.red : chalk.green;
+}
+
 export function formatOutput(data) {
   const { rank, symbol, name, priceUsd, changePercent24Hr } = data || {};
 
-  return `Coin: ${symbol} - (${name}) | Price: $${parseFloat(priceUsd).toFixed(
-    2,
-  )} | Rank: ${rank} | Change (24Hr): ${parseFloat(changePercent24Hr).toFixed(2)}%`;
+  const formattedSymbol = chalk.yellow(symbol);
+  const formattedPrice = chalk.green('$', parseFloat(priceUsd).toFixed(2));
+  const formattedChange = priceChangeColor(changePercent24Hr)(parseFloat(changePercent24Hr).toFixed(2) + '%');
+
+  return `Coin: ${formattedSymbol} - (${name}) | Price: ${formattedPrice} | Rank: ${chalk.blue(
+    rank,
+  )} | Change (24Hr): ${formattedChange}`;
 }
